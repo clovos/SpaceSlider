6fb7e18277e9a3dac585fd391586d7fb7585fa99
PJWoods <patrik.skoog83@gmail.com>
2016-01-30 16:46:04 +0100

    Testing and testing
    bajs

---
+++ New Text Document.bat
@@ -0,0 +1,6 @@
+echo "# codenameSpaceSlider" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/PJWoods/codenameSpaceSlider.git
+git push -u origin master
