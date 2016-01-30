f5193f9e22c7b6fbdf0b0d77b616739c6c5e5762
PJWoods <patrik.skoog83@gmail.com>
2016-01-30 21:17:11 +0100

    New Project!!!
    SpaceSlider is LIVE!

---
+++ SpaceSlider/Assets/CameraMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public bool ShowDebugInfo = false;
+    public Vector3 Velocity;
+	public float Acceleration;
+	private Vector3 m_currentVelocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		if(ShowDebugInfo)
+		{
+			if(Acceleration == 0)
+				Debug.LogError("The cameras acceleration is ZERO, resulting in no movement!");
+		}
+
+		float currentSpeed = m_currentVelocity.sqrMagnitude;
+		float targetSpeed = Velocity.sqrMagnitude;
+		if(Mathf.Abs(currentSpeed - targetSpeed) < 0.01f)
+		{
+			m_currentVelocity = Velocity;
+		}
+		Vector3 target = Velocity - m_currentVelocity;
+		m_currentVelocity += (target.normalized * Acceleration * Time.deltaTime);
+		transform.position += m_currentVelocity;
+	}
+	void OnGUI()
+	{
+		if(ShowDebugInfo)
+		{
+			Rect screenRectangle = new Rect(0, Screen.height - 20, Screen.width, 20.0f);
+			string text = "Camera position(" + transform.position.x.ToString() + ", " + transform.position.y.ToString() + ", " + transform.position.z.ToString() + ")"
+				+ ", velocity(" + m_currentVelocity.x.ToString() + ", " + m_currentVelocity.y.ToString() + ", " + m_currentVelocity.z.ToString() + ")";
+			GUI.Label(screenRectangle, text);
+
+			//Draw arrow
+			Vector3 rayPos = transform.position;
+			rayPos.x -= m_currentVelocity.x * 0.5f;
+			rayPos.y -= m_currentVelocity.y * 0.5f;
+			rayPos.z = Camera.main.nearClipPlane + 0.1f;
+
+			Vector3 direction = m_currentVelocity.normalized;
+			Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + 20, 0) * new Vector3(0, 1, 1);
+			Vector3 left = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 - 20, 0) * new Vector3(0, -1, 1);
+
+			Debug.DrawLine(rayPos, direction, new Color(1, 0, 0));
+			Debug.DrawRay(rayPos + direction, right * 0.25f, new Color(1, 0, 0));
+			Debug.DrawRay(rayPos + direction, left * 0.25f, new Color(1, 0, 0));
+		}
+	}
+
+}
---
+++ SpaceSlider/Assets/GameObjectPool.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameObjectPool : MonoBehaviour {
+
+	//Singleton
+	public static GameObjectPool Instance { get; private set; }
+	public bool ShowDebugInfo = false;
+
+	//A simple class that defines the prefab to hold in the pool and the amount of that gameobject
+	[Serializable]
+	public class PoolItem
+	{
+		public GameObject Prefab;
+		public int PoolSize = 0;
+	}
+
+	//List of prefabs to initiate the pool with
+	public PoolItem[] Prefabs;
+
+	//The actual list of all objects
+	public List<GameObject>[] m_poolOfObjects;
+
+	//The holding gameobject / the container
+	private GameObject m_containerObject;
+
+	void Awake ()
+	{
+		Instance = this;
+	}
+
+	void Start () 
+	{
+		m_containerObject = new GameObject("GameObjectPool");
+		m_poolOfObjects = new List<GameObject>[Prefabs.Length];
+
+		int prefabIndex = 0;
+		foreach (PoolItem poolItem in Prefabs)
+		{
+			Debug.Assert(poolItem.Prefab != null, "You need to specify a prefab / gameobject to initate the pool with, check element: " + prefabIndex.ToString());
+			Debug.Assert(poolItem.PoolSize > 0, "You tried to initiate a pool with size 0 with object type: " + poolItem.Prefab.name);
+			m_poolOfObjects[prefabIndex] = new List<GameObject>(); 
+
+			for (int n = 0; n < poolItem.PoolSize; ++n)
+			{
+				GameObject newObj = Instantiate(poolItem.Prefab) as GameObject;
+				newObj.name = poolItem.Prefab.name;
+				AddToPool(newObj);
+			}
+			++prefabIndex;
+		}
+	}
+
+	public GameObject GetFromPool(string objectType, bool instantiateIfEmpty)
+	{
+		for(int i = 0; i < Prefabs.Length; ++i)
+		{
+			PoolItem item = Prefabs[i];
+			if(item.Prefab.name == objectType)
+			{
+				if(m_poolOfObjects[i].Count > 0)
+				{
+					GameObject pooledObject = m_poolOfObjects[i][0];
+					m_poolOfObjects[i].RemoveAt(0);
+					pooledObject.transform.parent = null;
+					pooledObject.SetActive(true);
+					return pooledObject;
+				} 
+
+				if(instantiateIfEmpty) 
+				{
+					return Instantiate(m_poolOfObjects[i][0]) as GameObject;
+				}
+				break;
+
+			}
+		}
+		Debug.Assert(false, "The pool is empty and you didn't want to instantiate a new GameObject of type: " + objectType);
+		return null;
+	}
+
+	public void AddToPool(GameObject gameObject)
+	{
+		for (int i = 0; i < Prefabs.Length; ++i)
+		{
+			if(Prefabs[i].Prefab.name == gameObject.name)
+			{
+				gameObject.SetActive(false);
+				gameObject.transform.parent = m_containerObject.transform;
+				m_poolOfObjects[i].Add(gameObject);
+				return;
+			}
+		}
+		Debug.Assert(false, "You tried to add an object to the pool that isn't available! Object: " + gameObject.name);
+	}
+		
+	void Update () 
+	{
+
+	}
+
+	void OnGUI()
+	{
+		if(ShowDebugInfo)
+		{
+			Rect screenRectangle = new Rect(0, 0, 400.0f, 200.0f);
+			string text = "GameObject Pool:\n";
+
+			int totalAvailableObjects = 0;
+			int availablePerPrefabType = 0;
+			for(int i = 0; i < m_poolOfObjects.Length; ++i)
+			{
+				availablePerPrefabType = m_poolOfObjects[i].Count;
+				totalAvailableObjects += availablePerPrefabType;
+				text += m_poolOfObjects[i][0].name + ": " + availablePerPrefabType.ToString() + "\n";
+				availablePerPrefabType = 0;
+			}
+			text += "Total Available Objects: " + totalAvailableObjects.ToString();
+			GUI.Label(screenRectangle, text);
+		}
+	}
+}
