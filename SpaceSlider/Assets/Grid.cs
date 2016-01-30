75578b2e7a5c0c1dc52aebddcd4a964169263289
PJWoods <patrik.skoog83@gmail.com>
2016-01-30 23:09:02 +0100

    Grid
    Added grid

---
+++ SpaceSlider/Assets/Grid.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour {
+
+	[System.Serializable]
+	public class CellCount
+	{
+		public int X;
+		public int Y;
+	};
+
+	public class GridCell
+	{
+		public GridCell() { m_cellType = CellType.Empty; }
+		public enum CellType
+		{
+			Empty,
+			Movable,
+			NonMovable,
+			PowerUp,
+		};
+		private CellType m_cellType { get; set;}
+	};
+
+	public CellCount Cells;
+	public Vector2 CellDimensions;
+
+	private List<List<GridCell>> m_cells;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Assert(Cells.X > 0 && Cells.Y > 0 && CellDimensions.x > 0 && CellDimensions.y > 0, "INVALID GRID! Gör om gör rätt!");
+
+		m_cells = new List<List<GridCell>>(Cells.Y);
+		for (int y = 0; y < m_cells.Count; ++y) 
+		{
+			m_cells[y] = new List<GridCell>(Cells.X);
+			for (int x = 0; x < m_cells[y].Count; ++x) 
+			{
+				m_cells[y][x] = new GridCell();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ SpaceSlider/Assets/SpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+	public int SpawnDistance;
+	private Vector3 m_lastCameraPosition;
+
+	// Use this for initialization
+	void Start () {
+		m_lastCameraPosition = Camera.main.transform.position;
+		SpawnDistance *= SpawnDistance;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float sqLenght = (Camera.main.transform.position - m_lastCameraPosition).sqrMagnitude;
+		if(sqLenght > SpawnDistance)
+		{
+			m_lastCameraPosition = Camera.main.transform.position;
+	
+			Vector3 worldTopLeft = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, 0.0f));
+			Vector3 worldBottomRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0.0f));
+
+			int blockCount = Random.Range(0, 3);
+			for(int i = 0; i < blockCount; ++i)
+			{
+				string blockTypeId = "BlockType" + Random.Range(1, 3).ToString();
+				GameObject block = GameObjectPool.Instance.GetFromPool(blockTypeId, false);
+
+				worldBottomRight.y = worldTopLeft.y + ((worldTopLeft.y - worldBottomRight.y) / blockCount) * i;
+				block.transform.position = worldBottomRight;				
+			}
+		}
+	}
+}
